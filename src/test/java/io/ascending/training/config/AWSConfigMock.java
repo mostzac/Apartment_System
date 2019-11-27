8a183906a4c22430e12e0c8e2f88c398cf3ad076
mostzac <yangrunlai@vip.qq.com>
2019-11-27 13:48:43 -0500

    addMock

---
+++ src/test/java/io/ascending/training/config/AWSConfigMock.java
@@ -0,0 +1,20 @@
+package io.ascending.training.config;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import static org.mockito.Mockito.mock;
+
+@Configuration
+public class AWSConfiguration {
+    @Bean
+    @Primary
+    public AmazonS3 getAmazonS3(){
+        return mock(AmazonS3.class);
+    }
+
+}
