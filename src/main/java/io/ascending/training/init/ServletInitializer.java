a3eb97db5a65c336be9c5441d1e0a4277f0ef717
mostzac <yangrunlai@vip.qq.com>
2019-12-13 10:04:50 -0500

    tomcat

--- src/main/java/io/ascending/training/init/ServletInitializer.java
+++ src/main/java/io/ascending/training/init/ServletInitializer.java
@@ -1,12 +1,23 @@
 package io.ascending.training.init;
 
+import com.amazonaws.services.autoscaling.model.LoadBalancerTargetGroupState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+
 
 //WebSocket
+// if extends SpringBootServletInitializer, if using external tomcat, the embedded tomcat won't be used, if not use the embedded one.
+@Configuration
 public class ServletInitializer extends SpringBootServletInitializer {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(ApplicationBoot.class);
+        logger.info("SpringBootServletInitializer check point");
+        return builder.sources(ServletInitializer.class);
     }
 }
